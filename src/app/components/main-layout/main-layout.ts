031ec65c79ca9a281223c95617c2d58cf91593ab
Marco Antônio Duarte <marco.duoli2@gmail.com>
2026-07-10 11:59:35 -0300

    :recycle: refactor: organizando código e adicionando footer

--- src/app/components/main-layout/main-layout.ts
+++ src/app/components/main-layout/main-layout.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar';
 import { HeaderComponent } from '../header/header';
+import { FooterComponent } from '../footer/footer';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, SidebarComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, SidebarComponent, HeaderComponent, FooterComponent],
   templateUrl: './main-layout.html'
 })
 export class MainLayoutComponent {}
