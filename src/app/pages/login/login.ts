64608420fed88708dec08be5625403ab2092ac2f
Marco Antônio Duarte <marco.duoli2@gmail.com>
2026-07-10 00:28:02 -0300

    :construction: feat: Criando e adicionando a página de login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule], 
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./login.css']
 })
-export class Login {}
+export class Login {
+  loginForm: FormGroup;
+  erroLogin = false;
+  mostrarSenha = false; 
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required],
+      termos: [false, Validators.requiredTrue] 
+    });
+  }
+
+  toggleSenha(): void {
+    this.mostrarSenha = !this.mostrarSenha;
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, senha } = this.loginForm.value;
+      const sucesso = this.authService.login(email, senha);
+
+      if (sucesso) {
+        this.router.navigate(['/app/dashboard']);
+      } else {
+        this.erroLogin = true;
+      }
+    }
+  }
+}
